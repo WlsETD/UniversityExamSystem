4d29d6f4419b59f04c018592c9805b1b38a5a81b
WlsETD <william40510@gmail.com>
2025-12-07 18:21:07 +0800

    auto sync 2025-12-07_18-21-07

--- py/bb.java
+++ py/bb.java
@@ -3,11 +3,18 @@ import java.util.*;
 public class bb {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
+        int n = sc.nextInt();
 
-        int result = a + b * c - (a % b) - 1;
-        System.out.print(result);
+        for (int i = 1; i <= n; i++) {
+            int stars = 2 * i - 1; // 第 i 行要印的星星數
+
+            for (int j = 0; j < stars; j++) {
+                System.out.print("*");
+            }
+
+            if (i < n) { // 最後一行後面不要再多一個換行也可以
+                System.out.println();
+            }
+        }
     }
 }
