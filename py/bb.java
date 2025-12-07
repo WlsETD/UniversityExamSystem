620478e2233ee0f331bd0ad5b7a2b28eb62225ae
WlsETD <william40510@gmail.com>
2025-12-07 17:45:24 +0800

    auto sync 2025-12-07_17-45-24

--- py/bb.java
+++ py/bb.java
@@ -3,14 +3,12 @@ import java.util.*;
 public class bb {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int kwh = sc.nextInt();
-        int cost = 0;
-        if (kwh <= 100)
-            cost = kwh * 5;
-        else if (kwh <= 200)
-            cost = 100 * 5 + (kwh - 100) * 7;
-        else
-            cost = 100 * 5 + 100 * 7 + (kwh - 200) * 10;
-        System.out.print(cost);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int result = (a << 1) + (b >> 1) + (a ^ c) - (b % a);
+
+        System.out.print(result);
     }
 }
